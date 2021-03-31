87ab3d8fdc83823d9d3c42ed08876295c87025ba
Mudassar Shahbaz <59355787+mudasarshahbaz@users.noreply.github.com>
2021-03-31 23:16:00 +0500

    hot fixes

--- src/screens/NewPassword/index.js
+++ src/screens/NewPassword/index.js
@@ -49,7 +49,7 @@ export default class NewPassword extends Component {
 
     isPasswordValid(password) {
         // return /^(?=.*[0-9])(?=.*[!@#$%^&*])(?=.*[a-z])(?=.*[A-Z])[a-zA-Z0-9!@#$%^&*]{6,16}$/.test(password)
-        return /^(?=.*[0-9])(?=.*[!@#$%^&*])(?=.*[a-z])(?=.*[A-Z])[a-zA-Z0-9!@#$%^&*]{6,16}$/.test(password)
+        return /^(?=.*[0-9])(?=.*[a-z])(?=.*[A-Z])[a-zA-Z0-9!@#$%^&*]{6,16}$/.test(password)
     }
 
     render() {
