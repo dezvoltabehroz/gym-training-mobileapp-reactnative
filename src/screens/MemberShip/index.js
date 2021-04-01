725318709b93075aa89ce934f80c8526072a9759
Mudassar Shahbaz <59355787+mudasarshahbaz@users.noreply.github.com>
2021-04-01 12:03:55 +0500

    hot fixes

--- src/screens/MemberShip/index.js
+++ src/screens/MemberShip/index.js
@@ -96,7 +96,7 @@ class MemberShip extends Component {
     }
 
     hideDatePicker = () => {
-        this.setState({ showDatePicker: !this.state.showDatePicker, pauseMemberShip: true });
+        this.setState({ showDatePicker: !this.state.showDatePicker, });
     };
 
     handleConfirm = (selectedDate) => {
@@ -150,13 +150,146 @@ class MemberShip extends Component {
         const { name, email, phone, memberId, memberShipType, validFrom, validTo, pauseAvailed, date, loading, reason, selectedDuration, buttonLoading } = this.state;
         return (
             <>
-                { loading ?
-                    <View style={{ flex: 1, justifyContent: "center", alignItems: "center" }}>
-                        <ActivityIndicator size={30} color={THEME.PRIMARY_BACKGROUND_COLOR} />
-                    </View>
-                    :
-                    <View style={styles.container}>
-                        <View style={styles.memberShipContainer}>
+                {
+                    loading ?
+                        <View style={{ flex: 1, justifyContent: "center", alignItems: "center" }}>
+                            <ActivityIndicator size={30} color={THEME.PRIMARY_BACKGROUND_COLOR} />
+                        </View>
+                        :
+                        <View style={styles.container}>
+                            {
+                                this.state.pauseMemberShip ?
+                                    <View style={{ backgroundColor: "white", borderRadius: 8, marginTop: "10%", marginHorizontal: '2.5%', marginBottom: 2, }}>
+                                        <View style={{ marginTop: "5%", marginHorizontal: "5%", alignItems: "flex-end" }}>
+                                            <Icon.Entypo onPress={() => this.setState({ reason: "", date: new Date(), selectedDuration: [{}], pauseMemberShip: false, buttonLoading: false })} name="circle-with-cross" size={25} />
+                                        </View>
+                                        <View style={{ flexDirection: "row", justifyContent: "space-between", marginHorizontal: "5%", marginTop: '5%', alignItems: "center" }}>
+                                            <View style={{ flex: 0.5, }}>
+                                                <Text style={styles.userTextStyle}>Start Date</Text>
+                                                <TouchableOpacity style={styles.dateContainer} onPress={() => this.setState({ showDatePicker: true })}>
+                                                    <Text style={styles.dateTextStyle} >{moment(date).format("MMM DD,YYYY")}</Text>
+                                                    <Calender />
+                                                </TouchableOpacity>
+                                            </View>
+                                            <View style={{ width: 15 }}></View>
+                                            <View style={{ flex: 0.5, }}>
+                                                <Text style={styles.userTextStyle}>Time Duration</Text>
+                                                <View>
+                                                    <DropDownPicker
+                                                        items={[
+                                                            {
+                                                                id: 1,
+                                                                label: "1 Week",
+                                                                value: "1 Week",
+                                                            },
+                                                            {
+                                                                id: 2,
+                                                                label: "2 Week",
+                                                                value: "2 Week",
+                                                            },
+                                                            {
+                                                                id: 3,
+                                                                label: "3 Week",
+                                                                value: "3 Week",
+                                                            },
+                                                            {
+                                                                id: 4,
+                                                                label: "4 Week",
+                                                                value: "4 Week",
+                                                            }
+                                                        ]}
+                                                        arrowColor="#000000"
+                                                        placeholder="Select week"
+                                                        onClose={() => this.setState({ dropdownOpen: false })}
+                                                        onOpen={() => this.setState({ dropdownOpen: true })}
+                                                        containerStyle={{ height: 40, width: 140, marginBottom: this.state.dropdownOpen ? '50%' : 0 }}
+                                                        globalTextStyle={{ color: "#000000", fontSize: 12, fontFamily: "Montserrat-Medium" }}
+                                                        defaultValue={this.state.selectedDuration ? this.state.selectedDuration.label : ""}
+                                                        // containerStyle={{ height: 40 }}
+                                                        style={{ backgroundColor: 'white', marginTop: '5%' }}
+                                                        itemStyle={{
+                                                            justifyContent: 'center'
+                                                        }}
+                                                        dropDownStyle={{ backgroundColor: 'white' }}
+                                                        onChangeItem={(item) => {
+                                                            this.setState({
+                                                                selectedDuration: item, item: item.value, index: item.value,
+                                                            })
+                                                        }}
+                                                    />
+                                                </View>
+                                            </View>
+                                        </View>
+                                        <View style={{ marginHorizontal: '5%', marginTop: '3.5%' }}>
+                                            <Text style={{ fontSize: 12, fontFamily: 'Montserrat-Medium', fontWeight: "normal", }} >Any specific reason? (Required)</Text>
+                                            <View style={{ marginTop: "5%" }}>
+                                                <Input
+                                                    placeholder="Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam."
+                                                    multiline={true}
+                                                    value={reason}
+                                                    containerStyle={styles.containerStyle}
+                                                    placeholderTextColor={'#77777B'}
+                                                    onChangeText={(reason) => this.setState({ reason })}
+                                                    inputContainerStyle={styles.inputContainerStyle}
+                                                    inputStyle={styles.inputStyle} />
+                                            </View>
+                                        </View>
+                                        <View style={{ alignItems: 'flex-end', marginTop: '5%', marginHorizontal: "5%", paddingBottom: '5%' }}>
+                                            <Button loading={buttonLoading} disabled={reason && selectedDuration.value && date ? false : true} titleStyle={buttonStyle.colorBtnPrimaryText} buttonStyle={styles.colorBtnPrimary} title='Request Pause ' onPress={() => this.handleRequestPause()} />
+                                        </View>
+                                    </View>
+                                    :
+                                    <>
+                                        <View style={styles.memberShipContainer}>
+                                            <View style={styles.memberShipContentRowStyle}>
+                                                <View style={styles.memberShipContentRow}>
+                                                    <View >
+                                                        <User />
+                                                    </View>
+                                                    <View style={{ marginLeft: 5 }}>
+                                                        <Text style={styles.userDetailTextStyle}>{name}</Text>
+                                                        <Text style={styles.userDetailTextStyle}>{email}</Text>
+                                                        <Text style={styles.userDetailTextStyle}>{phone}</Text>
+                                                    </View>
+                                                </View>
+                                                <View>
+                                                    <Text style={styles.memberShipTypeTextStyle}>{memberShipType}</Text>
+                                                </View>
+                                            </View>
+                                            <View style={styles.memberShipIdContainer}>
+                                                <Text style={styles.userDetailTextStyle}>Member ID : {memberId}</Text>
+                                            </View>
+                                            <View style={[styles.memberShipContentRowStyle, { marginTop: "5%" }]}>
+                                                <View style={{ flexDirection: "row" }}>
+                                                    <Text style={styles.validityTextStyle}>{"VALID\nFROM"}</Text>
+                                                    <Text style={styles.userDetailTextStyle}>{validFrom}</Text>
+                                                </View>
+                                                <View style={{ flexDirection: "row" }}>
+                                                    <Text style={styles.validityTextStyle}>{"VALID\nTHRU"}</Text>
+                                                    <Text style={styles.userDetailTextStyle}>{validTo}</Text>
+                                                </View>
+                                            </View>
+                                        </View>
+                                        <View style={styles.pausedAvailedContainer}>
+                                            <View style={styles.memberShipContentRowStyle}>
+                                                <Text style={styles.userDetailTextStyle}>Paused Availed</Text>
+                                                <Text style={styles.userDetailTextStyle}>{pauseAvailed}</Text>
+                                            </View>
+
+                                        </View>
+                                        <View style={styles.pausedHistoryContainer}>
+                                            <TouchableOpacity onPress={() => this.props.navigation.navigate("PauseHistory", { memberId })} style={styles.memberShipContentRowStyle}>
+                                                <Text style={styles.userDetailTextStyle}>Paused History</Text>
+                                                <Icon.Entypo name={"chevron-right"} size={20} />
+                                            </TouchableOpacity>
+                                        </View>
+                                        <View style={{ alignItems: 'flex-end', marginTop: '15%', marginHorizontal: "5%" }}>
+                                            <Button titleStyle={buttonStyle.colorBtnPrimaryText} buttonStyle={styles.colorBtnPrimary} title='Pause Membership ' onPress={() => this.setState({ pauseMemberShip: true })} />
+                                        </View>
+                                    </>
+                            }
+                        </View>
+                        /* <View style={styles.memberShipContainer}>
                             <View style={styles.memberShipContentRowStyle}>
                                 <View style={styles.memberShipContentRow}>
                                     <View >
@@ -283,7 +416,7 @@ class MemberShip extends Component {
                             <Button loading={buttonLoading} disabled={reason && selectedDuration.value && date ? false : true} titleStyle={buttonStyle.colorBtnPrimaryText} buttonStyle={styles.colorBtnPrimary} title='Request Pause ' onPress={() => this.handleRequestPause()} />
                         </View>
                     </View>
-                </Modal>
+                </Modal> */}
                 <DateTimePickerModal
                     isVisible={this.state.showDatePicker}
                     mode="date"
